4b12fb76d4e6c84b98526b713538ae3bc0e9fcae
BowangLan <60339191+BowangLan@users.noreply.github.com>
2024-08-02 00:33:46 -0700

    Update create-cookie route to set multiple cookies

--- app/create-cookie/route.ts
+++ app/create-cookie/route.ts
@@ -1,12 +1,13 @@
 export const GET = (request: Request) => {
   const c1 = "cookie1=cookieValue; Secure; HttpOnly; SameSite=Strict"
   const c2 = "cookie2=cookieValue2; Secure; HttpOnly; SameSite=Strict"
+  const headers = new Headers()
+  headers.set("Set-Cookie", c1)
+  headers.append("Set-Cookie", c2)
+  headers.set("Location", "/view-cookie")
   const res = new Response("Hello Worker", {
     status: 302,
-    headers: {
-      Location: "/view-cookie",
-      "Set-Cookie": [c1, c2].join(","),
-    },
+    headers,
   })
 
   return res
