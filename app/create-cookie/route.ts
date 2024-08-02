8ff1269e4cd1e5dd2a77dc4bf67d9d7180188e03
BowangLan <60339191+BowangLan@users.noreply.github.com>
2024-08-02 00:49:47 -0700

    Update create-cookie route to use Next.js server-side rendering

--- app/create-cookie/route.ts
+++ app/create-cookie/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server"
+
 export const GET = (request: Request) => {
   const c1 = "cookie1=cookieValue; Secure; HttpOnly; SameSite=Strict"
   const c2 = "cookie2=cookieValue2; Secure; HttpOnly; SameSite=Strict"
@@ -5,10 +7,8 @@ export const GET = (request: Request) => {
   headers.set("Set-Cookie", c1)
   headers.append("Set-Cookie", c2)
   headers.set("Location", "/view-cookie")
-  const res = new Response("Hello Worker", {
-    status: 302,
+  
+  return NextResponse.redirect("/view-cookie", {
     headers,
   })
-
-  return res
 }
