65b32ddd2200ea54e901a5e2b54af1ce38370aa2
BowangLan <60339191+BowangLan@users.noreply.github.com>
2024-08-02 01:03:33 -0700

    chore: Update create-cookie route to set multiple cookies using Next.js server-side rendering

--- app/create-cookie/route.ts
+++ app/create-cookie/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export const GET = () => {
+  const headers = new Headers();
+  headers.set("Set-Cookie", "cookie1=VALUE1; Path=/; HttpOnly");
+  headers.append("Set-Cookie", "cookie2=VALUE2; Path=/; HttpOnly");
+  headers.append("Location", "/view-cookie");
+
   const res = new NextResponse(null, {
     status: 302,
-    headers: {
-      Location: "/view-cookie",
-    },
+    headers,
   })
-  res.cookies.set("cookie1", "cookieValue1")
-  res.cookies.set("cookie2", "cookieValue2")
 
   return res
 }
